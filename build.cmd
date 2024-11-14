e2191d5598d44d6a6b2e59a70c859f21a1224897
Omena0 <Omena0MC@gmail.com>
2024-11-14 21:25:59 +0200

    a

---
+++ build.cmd
@@ -0,0 +1,8 @@
+
+py -m nuitka --standalone --onefile --windows-console-mode=disable --enable-plugin=tk-inter^
+ --product-name="Image overlay" --product-version=1.0.0 --file-description="Overlays an image on top of your screen frfr" --copyright="Copyright © 2024 Omena0. All rights reserved."^
+ --output-dir="__build"^
+ --deployment --python-flag="-OO" --python-flag="-S"^
+ --output-filename="imageOverlay.exe"^
+ --nofollow-import-to=nuitka^
+ main.py
---
+++ main.py
@@ -0,0 +1,48 @@
+from PIL import Image, ImageTk
+import tkinter
+import sys
+
+root = tkinter.Tk()
+root.title('Overlay thingy')
+root.geometry('300x100')
+
+window_width = 300
+window_height = 100
+
+root.wm_attributes("-transparentcolor", root['bg'], '-topmost', True)
+
+original_img = Image.open(sys.argv[1])
+
+original_img.point
+
+def convert(img:Image, size):
+    new_img = img.copy()
+    new_img:Image.Image = new_img.resize(size)
+
+    # Remove pixels that are not close to pure white
+    new_img = new_img.point(lambda p: 255 if p > 170 else 0)
+    return ImageTk.PhotoImage(new_img)
+
+def update_image(event):
+    global window_width, window_height
+    if (window_width == event.width) and (window_height == event.height):
+        return
+
+    window_width = event.width
+    window_height = event.height
+
+    global img
+    img = convert(original_img, (root.winfo_width(), root.winfo_height()))
+    canvas.delete('all')
+    canvas.create_image(0, 0, image=img, anchor='nw')
+
+canvas = tkinter.Canvas(root, bg=root['bg'], width=10000, height=1000)
+canvas.pack()
+
+img = convert(original_img, (300, 100))
+canvas.create_image(0, 0, image=img, anchor='nw')
+
+root.bind('<Configure>', update_image)
+
+root.mainloop()
+
